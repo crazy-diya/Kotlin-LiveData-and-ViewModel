21a0146c5ec06ad6c5f1568b94ec26d0e891f272
dimuthu_l <dimuthu_l@epiclanka.net>
2022-05-29 00:34:21 +0530

    Live data working system

--- app/src/main/java/com/ktl1/viewmodelapplication/MainActivity.kt
+++ app/src/main/java/com/ktl1/viewmodelapplication/MainActivity.kt
@@ -3,13 +3,15 @@ package com.ktl1.viewmodelapplication
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel : ViewModelMainActivity // this is important
-//    private var viewModel = ViewModelMainActivity()
+    //    private var viewModel = ViewModelMainActivity()
+    private lateinit var viewModel: ViewModelMainActivity // this is important
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +19,17 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this).get(ViewModelMainActivity::class.java)
 
         val text = findViewById<TextView>(R.id.textView)
+        val button = findViewById<FloatingActionButton>(R.id.floatingActionButton2)
 
-        text.text = viewModel.count.toString() // this is important
+//        text.text = viewModel.count.toString() // this is important
 
-        val button =findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+        viewModel.count.observe(this, Observer {
+            text.text = it.toString()
+        })
 
-        button.setOnClickListener{
+        button.setOnClickListener {
             viewModel.increment() // this is important
-            text.text = viewModel.count.toString() // this is important
+//            text.text = viewModel.count.toString() // this is important
         }
     }
 }
--- app/src/main/java/com/ktl1/viewmodelapplication/ViewModelMainActivity.kt
+++ app/src/main/java/com/ktl1/viewmodelapplication/ViewModelMainActivity.kt
@@ -1,11 +1,23 @@
 package com.ktl1.viewmodelapplication
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ViewModelMainActivity : ViewModel(){
-     var count = 0
+    /*    var count : Int = 0
+*/
+//     var count = 0
+//
+//    fun increment() : Int{
+//        return count++
+//    }
 
-    fun increment() : Int{
-        return count++
+    var count = MutableLiveData<Int>()
+
+    init {
+        count.value = 0
+    }
+    fun increment(){
+        count.value = (count.value)?.plus(1)
     }
 }
